4c35a037db86314a81f250a1e66628da2bf1ea81
Nicolas Dickreuter <dickreuter@gmail.com>
2025-05-15 16:50:22 +0100

    30 days fre3e trial button

--- app/bidstats/page.tsx
+++ app/bidstats/page.tsx
@@ -11,6 +11,7 @@ import LayoutWithBorders from "@/components/ui/LayoutWithBorder";
 import Slider from "@/components/ui/slider";
 import SliderMobile from "@/components/ui/slider-mobile";
 import styles from "./bidstats.module.css";
+import Link from "next/link";
 
 export default function BitStats() {
   // Create an array of banner messages that will be duplicated to ensure continuous flow
@@ -34,6 +35,16 @@ export default function BitStats() {
             </p>
           ))}
         </div>
+      </div>
+      
+      {/* Free trial button */}
+      <div className={styles.buttonContainer}>
+        <Link href="https://buy.stripe.com/eVa03xfjPd8Egq44gs" className={styles.freeTrialButton}>
+          Enjoy 30 days free
+          <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M9.7 18.3c-.2.2-.4.3-.7.3-.3 0-.5-.1-.7-.3-.4-.4-.4-1 0-1.4L13.6 12 8.3 6.7c-.4-.4-.4-1 0-1.4.4-.4 1-.4 1.4 0l6 6c.4.4.4 1 0 1.4l-6 5.6z" fill="white"/>
+          </svg>
+        </Link>
       </div>
       
       {/* Same content as the main page */}
